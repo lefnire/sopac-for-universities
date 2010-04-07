362df5eb10d43eedb1baf37ea4f836087a65e695
Tyler Renelle <tylerrenelle@gmail.com>
2010-04-07 11:32:41 -0700

    collapsible table rows, instead of fieldset

---
+++ theme_overrides/js/ucsf_sopac.js
@@ -0,0 +1,30 @@
+
+// Collapses large tables of book status/locations, with a "show more" link
+Drupal.behaviors.sopacCollapseLocationsTable = function (context) {
+    var max_rows = 3;
+    var table = $("table#sopac-status-location");
+    var rows = table.find("tr");
+    
+    if(rows.length>1){
+      var tbody = rows
+        .slice(max_rows)
+        .wrapAll("<tbody id='sopac-hidden-locations'></tbody>")
+        .parent()
+          .appendTo( table )
+          .hide();
+          
+      var show_text = "[+] Show all locations ("+ (rows.length-max_rows) +")";
+      var hide_text = "[-] Hide all locations ("+ (rows.length-max_rows) +")";
+      table
+        .after("<input id='sopac-show-locations' type='button' value='"+ show_text +"'/>")
+        .next()
+          .click(function() {
+            if(tbody.is(":visible")){
+              $(this).val(show_text);
+            }else{
+              $(this).val(hide_text);
+            }
+            tbody.toggle();
+          });
+    }
+};
